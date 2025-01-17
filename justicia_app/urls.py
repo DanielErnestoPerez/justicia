6ae0db92a632cd9aa197f1a1aed4fab78ef771c6
DanielErnestoPerez <124157524+DanielErnestoPerez@users.noreply.github.com>
2025-01-17 12:29:32 +0800

    Update urls.py

--- justicia_app/urls.py
+++ justicia_app/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('publicaciones/', views.publicaciones, name='publicaciones'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
