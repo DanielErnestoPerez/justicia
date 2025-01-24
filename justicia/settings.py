4852d9267c1b299670dd83435b1a7b8c6cf9cbdb
DanielErnestoPerez <124157524+DanielErnestoPerez@users.noreply.github.com>
2025-01-24 17:36:15 +0800

    Update settings.py

--- justicia/settings.py
+++ justicia/settings.py
@@ -38,7 +38,7 @@ else:
     }
     
 
-ALLOWED_HOSTS = ['justicia-hgin.onrender.com', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', 'justicia-hgin.onrender.com', '127.0.0.1']
 
 INTERNAL_IPS = (
     '127.0.0.1',
