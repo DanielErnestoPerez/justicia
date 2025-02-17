474e54634f24e3c56c710f33590672208714336f
DanielErnestoPerez <124157524+DanielErnestoPerez@users.noreply.github.com>
2025-02-17 11:15:07 +0800

    inbox model create

--- inbox/views.py
+++ inbox/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-
+from .models import InboxMessage, Conversation
 # Create your views here.
 
 @login_required
 def inbox(request):
-    return render(request, 'inbox/inbox.html')
+    conversation = Conversation.objects.first()
+    context = {
+        'conversation': conversation,
+    }
+    return render(request, 'inbox/inbox.html', context)
