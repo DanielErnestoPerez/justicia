5d0ae58f74926c2ac0667630335385a4ace65193
DanielErnestoPerez <124157524+DanielErnestoPerez@users.noreply.github.com>
2025-02-20 16:19:15 +0800

    Update urls.py

--- inbox/urls.py
+++ inbox/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.inbox, name='inbox'),
-    path('<conversation_id>/', views.inbox, name='inbox'),
     path('search_users/', views.search_users, name='inbox_search_users'),
+    path('<conversation_id>/', views.inbox, name='inbox'),
     path('new_message/<recipient_id>/', views.new_message, name='inbox_new_message'),
     path('new_reply/<conversation_id>/', views.new_reply, name='inbox_new_reply'),
 ]
